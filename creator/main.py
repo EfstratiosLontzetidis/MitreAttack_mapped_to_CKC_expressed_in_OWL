1dda65167d90c3e73cf83fee9107d87aae7641e2
asdimitriadis <asdimitriadis@uom.edu.gr>
2021-08-14 10:05:07 -0400

    remove unnecessary comments

--- creator/main.py
+++ creator/main.py
@@ -6,25 +6,25 @@ from owlready import *
 # A new empty ontology is created. The ontology is called attack. The IRI of the ontology is also given
 attack = Ontology("https://raw.githubusercontent.com/EfstratiosLontzetidis/AttackOWL/master/attack.owl")
 
-# # create the class data_sources_class with the OWL name "data-sources" dynamically
-# data_sources_class = types.new_class("data-sources", (Thing,), kwds={"ontology": attack})
-#
-# # set the path of the yaml file that contains the data sources and their components
-# yaml_data_sources_file_path = os.path.join(os.getcwd(),"..\\enterprise-attack\\data-sources\\attack-data-sources.yaml")
-#
-# # open yaml file
-# with open(yaml_data_sources_file_path, "r", encoding="utf-8") as data_sources_file:
-#     # for each data source in the data_sources_file
-#     for data_source in yaml.safe_load(data_sources_file):
-#         # create the class a_data_source_class. Use the -name field to set the name
-#         a_data_source_class = types.new_class(data_source['name'], (data_sources_class,), kwds={"ontology": attack})
-#         # for each data component of the specific data source
-#         for data_component in data_source['data_components']:
-#             # print(data2['name'])
-#             # create the class a_data_component_class. Use the -name field to set the name
-#             # this is a data component that belongs to the data source - so it is a subclass - here it is declared
-#             a_data_component_class = types.new_class(data_component['name'], (a_data_source_class,), kwds={"ontology": attack})
-#         #print("============================================================================")
+# create the class data_sources_class with the OWL name "data-sources" dynamically
+data_sources_class = types.new_class("data-sources", (Thing,), kwds={"ontology": attack})
+
+# set the path of the yaml file that contains the data sources and their components
+yaml_data_sources_file_path = os.path.join(os.getcwd(),"..\\enterprise-attack\\data-sources\\attack-data-sources.yaml")
+
+# open yaml file
+with open(yaml_data_sources_file_path, "r", encoding="utf-8") as data_sources_file:
+    # for each data source in the data_sources_file
+    for data_source in yaml.safe_load(data_sources_file):
+        # create the class a_data_source_class. Use the -name field to set the name
+        a_data_source_class = types.new_class(data_source['name'], (data_sources_class,), kwds={"ontology": attack})
+        # for each data component of the specific data source
+        for data_component in data_source['data_components']:
+            # print(data2['name'])
+            # create the class a_data_component_class. Use the -name field to set the name
+            # this is a data component that belongs to the data source - so it is a subclass - here it is declared
+            a_data_component_class = types.new_class(data_component['name'], (a_data_source_class,), kwds={"ontology": attack})
+        #print("============================================================================")
 
 
 # somewhere inside attack pattern there will be the relationships (object properties) and properties (data properties) beside classes
@@ -34,14 +34,12 @@ directory_of_attack_patterns = os.path.join(os.getcwd(), "..\\enterprise-attack\
 # create an attack pattern class
 attack_pattern_class=types.new_class("attack-pattern", (Thing,), kwds={"ontology": attack})
 
-
 # read all attack-pattern stix files
-count=0
+# count=0
 # for each attack pattern file that exists in the directory
 for attack_pattern_file in os.listdir(directory_of_attack_patterns):
     # open the file
     with open(os.path.join(directory_of_attack_patterns, attack_pattern_file), "r", encoding="utf-8") as file_handle:
-        count=count+1
         # parse the file as a stix object and create the class
         stix_object = parse(file_handle, allow_custom=True)
         # if the attack pattern is not revoked and not deprecated
@@ -57,13 +55,13 @@ for attack_pattern_file in os.listdir(directory_of_attack_patterns):
 # properties example, in owlready object properties and data properties are the same ( in data -> range = str )
 domainlist=[]
 domainlist.append(attack_pattern_class)
-# rangelist=[]
-# rangelist.append(data_sources_class)
+rangelist=[]
+rangelist.append(data_sources_class)
 class has(Property):
     # domain and range must be lists
     ontology = attack
-    domain = domainlist
-    # range = rangelist
+    # domain = domainlist
+    range = rangelist
 # print(attackpatternclass)
 
 attack.save(filename="..\\attack.owl")
