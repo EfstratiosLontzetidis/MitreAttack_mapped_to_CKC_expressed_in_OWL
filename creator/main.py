a9beba65cba92810249008ed07ecf5333328165e
slont <s.lontzetidis@gmail.com>
2021-08-16 13:02:32 +0300

    #1 solved

--- creator/main.py
+++ creator/main.py
@@ -29,44 +29,48 @@ attack = Ontology("https://raw.githubusercontent.com/EfstratiosLontzetidis/Attac
 
 # somewhere inside attack pattern there will be the relationships (object properties) and properties (data properties) beside classes
 # set the directory of the attack-patterns
-directory_of_attack_patterns = os.path.join(os.getcwd(), "..\\enterprise-attack\\attack-pattern")
+directory_of_attack_patterns = os.path.join(os.getcwd(), ".\\enterprise-attack\\attack-pattern")
 
 # create an attack pattern class
 attack_pattern_class=types.new_class("attack-pattern", (Thing,), kwds={"ontology": attack})
 
 
 # read all attack-pattern stix files
-count=0
 # for each attack pattern file that exists in the directory
 for attack_pattern_file in os.listdir(directory_of_attack_patterns):
     # open the file
     with open(os.path.join(directory_of_attack_patterns, attack_pattern_file), "r", encoding="utf-8") as file_handle:
-        count=count+1
         # parse the file as a stix object and create the class
         stix_object = parse(file_handle, allow_custom=True)
         # if the attack pattern is not revoked and not deprecated
-        if not is_revoked(stix_object) and not is_deprecated(stix_object):
-            # print(get_name_of_attack_pattern(stix_object).replace(" ", "_").replace("/", "_"))
-            # print(''.join(filter(str.isalnum, get_name_of_attack_pattern(stix_object))))
-            techniqueclass = types.new_class(get_name_of_attack_pattern(stix_object).replace(" ", "_").replace("/", "_"), (attack_pattern_class,), kwds={"ontology": attack})
-            # print(get_name_of_attack_pattern(stix_object) + "    " +get_id_of_attack_pattern(stix_object))
-        # get_data_sources_of_attack_pattern(stix_object)
-        # count=count+1
-        # print(count)
+        if is_revoked(stix_object)==False and is_deprecated(stix_object)==False and is_subtechnique(stix_object)==False:
+            techniqueclass = types.new_class(
+                get_name_of_attack_pattern(stix_object).replace(" ", "_").replace("/", "_"),
+                (attack_pattern_class,), kwds={"ontology": attack})
+            for attack_pattern_file2 in os.listdir(directory_of_attack_patterns):
+                with open(os.path.join(directory_of_attack_patterns, attack_pattern_file2), "r",
+                          encoding="utf-8") as file_handle2:
+                    stix_object2 = parse(file_handle2, allow_custom=True)
+                    # if the attack pattern is not revoked and not deprecated
+                    if is_revoked(stix_object2)==False and is_deprecated(stix_object2)==False and is_subtechnique(stix_object2)==True:
+                        if get_id_of_attack_pattern(stix_object) in get_id_of_attack_pattern(stix_object2):
+                            subtechniqueclass = types.new_class(
+                                get_name_of_attack_pattern(stix_object2).replace(" ", "_").replace("/", "_"),
+                                (techniqueclass,), kwds={"ontology": attack})
 
-# properties example, in owlready object properties and data properties are the same ( in data -> range = str )
-domainlist=[]
-domainlist.append(attack_pattern_class)
-# rangelist=[]
-# rangelist.append(data_sources_class)
-class has(Property):
-    # domain and range must be lists
-    ontology = attack
-    domain = domainlist
-    # range = rangelist
-# print(attackpatternclass)
+# # properties example, in owlready object properties and data properties are the same ( in data -> range = str )
+# domainlist=[]
+# domainlist.append(attack_pattern_class)
+# # rangelist=[]
+# # rangelist.append(data_sources_class)
+# class has(Property):
+#     # domain and range must be lists
+#     ontology = attack
+#     domain = domainlist
+#     # range = rangelist
+# # print(attackpatternclass)
 
-attack.save(filename="..\\attack.owl")
+attack.save(filename=".\\attack.owl")
 
 
 
